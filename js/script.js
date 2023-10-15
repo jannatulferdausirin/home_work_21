9249b009cd80c02544b11437e67e816d79eded88
jannatulferdous <jannatulferdausirin1@gmail.com>
2023-10-15 20:14:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// querySelectors=============
+// let text=document.querySelector('h1');
+// let button=document.querySelector('button');
+// button.addEventListener("click",function(){
+//     text.style.color="blue";
+//     text.style.background="gold";
+// })
+
+// ==================function=========
+$("#addColor").click(function(){
+    $('.box').addClass('box_two');
+})
+$("#removeColor").click(function(){
+    $('.box').removeClass('box_two');
+})
+$("#addremoveColor").click(function(){
+    $('.box').toggleClass('box_two');
+})
+$("#hide").click(function(){
+    $('.box').hide(100);
+})
+$("#show").click(function(){
+    $('.box').show(100);
+})
+$("#hideShow").click(function(){
+    $('.box').toggle(200);
+})
+$("#slideUp").click(function(){
+    $('.box').slideUp(2000);
+})
+$("#slideDown").click(function(){
+    $('.box').slideDown(2000);
+})
