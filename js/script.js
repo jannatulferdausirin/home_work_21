c80be29561a5ea65c4f832504d6482152fb4fb0e
jannatulferdous <jannatulferdausirin1@gmail.com>
2023-10-17 05:29:19 +0300

    remove name color added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 // querySelectors=============
 let text=document.querySelector('h1');
 let button=document.querySelector('button');
+let a=document.querySelector('buttonChange');
 button.addEventListener("click",function(){
     text.style.color="white";
     text.style.background="olivedrab";
    
     text.style.fontSize="50px";
+    text.style.paddingBottom="10px";
+    text.style.marginBottom="10px";
+})
+buttonChange.addEventListener("click",function(){
+    text.style.color="";
+    text.style.background="";
+    text.style.fontSize="";
 })
 
 // ==================function=========
@@ -19,10 +27,10 @@ $("#addremoveColor").click(function(){
     $('.box').toggleClass('box_two');
 })
 $("#hide").click(function(){
-    $('.box').hide(100);
+    $('.box').hide();
 })
 $("#show").click(function(){
-    $('.box').show(100);
+    $('.box').show();
 })
 $("#hideShow").click(function(){
     $('.box').toggle(200);
